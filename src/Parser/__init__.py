f667b36be9b6f0bd122688dde87980b10878924a
Igor F. Yago <igor.yago@gmail.com>
2018-10-19 00:45:07 +0100

    fixed bug when more than 1 digit in color

--- src/Parser/__init__.py
+++ src/Parser/__init__.py
@@ -1,21 +1,22 @@
 from collections import namedtuple
 
-trim = lambda line: line.rstrip('\n').replace(' ', '').replace('\t', '').strip()
-combine_every_n = lambda line, n: (line[i:i + n] for i in range(0, len(line), n))
-preferences = lambda line: dict((int(string[0]), str(string[1])) for string in combine_every_n(line, 2))
+trim = lambda line: (line.rstrip('\n').replace(' ', '').replace('\t', '').strip())
+split_preferences = lambda line: line.replace('G', 'G ').replace('M', 'M ').rstrip(' ').split(' ')
+preferences = lambda line: dict((int(string[:-1]), str(string[-1])) for string in split_preferences(line))
 
 
 class Parser:
 
     def __init__(self, path):
+
         self.path = str(path)
 
         self.input_lines = [trim(line) for line in open(self.path, 'r').read().splitlines() if len(trim(line)) > 0]
 
         self.styles_solution = {k: 'G' for k, v in ((color_i + 1, 'G') for color_i in range(int(self.input_lines[0])))}
 
-        self.Customers = namedtuple('Customers', ['id', 'preferences', 'preferences_count'])
+        self.Customers = namedtuple('Customers', ['preferences', 'preferences_count'])
 
         self.customers = sorted(
-            [self.Customers(id=row, preferences=preferences(line), preferences_count=len(preferences(line))) for row, line in enumerate(self.input_lines[1:])]
+            tuple(self.Customers(preferences=preferences(line), preferences_count=len(preferences(line))) for line in self.input_lines[1:])
             , key=lambda customer: customer.preferences_count)
